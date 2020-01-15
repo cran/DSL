11307be346ab9e825d70151f6b4ea6f3945a7813
Stefan Theussl <Stefan.Theussl@R-project.org>
2020-01-15 05:40:02 +0000

    version 0.1-7

--- src/init.c
+++ src/init.c
@@ -1,10 +1,8 @@
 #include <R.h>
 #include <Rinternals.h>
-#include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* .Call calls */
-extern SEXP _collector2(SEXP, SEXP);
+SEXP _collector2(SEXP x, SEXP y);
 
 static const R_CallMethodDef CallEntries[] = {
     {"_collector2", (DL_FUNC) &_collector2, 2},
